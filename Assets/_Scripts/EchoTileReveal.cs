f1122b9a207b5f07477fa736066b79bb2142e2a3
wizardchaitanya <chaitanyakeny@gmail.com>
2026-02-06 23:41:51 +0530

    Working Echo Wave
    The echo wave (sound wave) working perfectly when activated it revels the objects and tiles also.

---
+++ Assets/_Scripts/EchoTileReveal.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections.Generic;
+
+public class EchoTileReveal : MonoBehaviour
+{
+    Tilemap tilemap;
+
+    Dictionary<Vector3Int, float> revealedTiles = new Dictionary<Vector3Int, float>();
+    List<Vector3Int> pendingReveal = new List<Vector3Int>();
+
+    public float revealDuration = 1.2f;
+
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+
+        // Make every tile invisible at start
+        foreach (var pos in tilemap.cellBounds.allPositionsWithin)
+        {
+            if (tilemap.HasTile(pos))
+            {
+                tilemap.SetTileFlags(pos, TileFlags.None);
+                tilemap.SetColor(pos, new Color(1, 1, 1, 0));
+            }
+        }
+    }
+
+    void Update()
+    {
+        // APPLY queued reveals
+        if (pendingReveal.Count > 0)
+        {
+            foreach (var cell in pendingReveal)
+            {
+                SetTileAlpha(cell, 1f);
+                revealedTiles[cell] = revealDuration;
+            }
+            pendingReveal.Clear();
+        }
+
+        // 🔒 SAFE ITERATION: COPY KEYS
+        var keys = new List<Vector3Int>(revealedTiles.Keys);
+
+        foreach (var cell in keys)
+        {
+            revealedTiles[cell] -= Time.deltaTime;
+
+            if (revealedTiles[cell] <= 0f)
+            {
+                SetTileAlpha(cell, 0f);
+                revealedTiles.Remove(cell);
+            }
+        }
+    }
+
+    void SetTileAlpha(Vector3Int cellPos, float alpha)
+    {
+        tilemap.SetTileFlags(cellPos, TileFlags.None);
+        tilemap.SetColor(cellPos, new Color(1, 1, 1, alpha));
+    }
+
+    // 🚨 IMPORTANT: this no longer touches the dictionary directly
+    public void RevealTile(Vector3Int cellPos)
+    {
+        if (!pendingReveal.Contains(cellPos))
+            pendingReveal.Add(cellPos);
+    }
+}
--- Assets/_Scripts/EchoWave.cs
+++ Assets/_Scripts/EchoWave.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class EchoWave : MonoBehaviour
 {
     public float speed = 10f;
     public float maxSize = 15f;
 
+    EchoWave wave;
+    EchoTileReveal tileReveal;
+    Tilemap tilemap;
+
     float size;
+
+    void Start()
+    {
+        wave = GetComponent<EchoWave>();
+        tileReveal = FindObjectOfType<EchoTileReveal>();
+        tilemap = tileReveal.GetComponent<Tilemap>();
+    }
 
     void Update()
     {
@@ -15,7 +27,34 @@ public class EchoWave : MonoBehaviour
 
         transform.localScale = Vector3.one * size;
 
+        RevelTiles();
+
         if (size >= maxSize)
             Destroy(gameObject);
+    }
+
+    void RevelTiles()
+    {
+        float radius = transform.localScale.x / 2f;
+
+        Vector3Int centerCell = tilemap.WorldToCell(transform.position);
+        int cellRadius = Mathf.CeilToInt(radius / tilemap.cellSize.x);
+
+        for (int x = -cellRadius; x <= cellRadius; x++)
+        {
+            for (int y = -cellRadius; y <= cellRadius; y++)
+            {
+                Vector3Int cell = centerCell + new Vector3Int(x, y, 0);
+                Vector3 worldPos = tilemap.GetCellCenterWorld(cell);
+
+                if (Vector2.Distance(worldPos, transform.position) <= radius)
+                {
+                    if (tilemap.HasTile(cell))
+                    {
+                        tileReveal.RevealTile(cell);
+                    }
+                }
+            }
+        }
     }
 }
