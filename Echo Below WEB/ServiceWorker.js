5c87b9fb3d8ee5f5f3b7d582d745b73557d3ddd3
wizardchaitanya <chaitanyakeny@gmail.com>
2026-02-09 02:15:36 +0530

    Switch to WEB build

---
+++ Echo Below WEB/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Echo Below-1.0";
+const contentToCache = [
+    "Build/Echo Below WEB.loader.js",
+    "Build/Echo Below WEB.framework.js.br",
+    "Build/Echo Below WEB.data.br",
+    "Build/Echo Below WEB.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
